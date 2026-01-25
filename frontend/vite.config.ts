767b6591d5ee89a6b54d619ec96e47f6ccb5bc79
Bang Xiao <bangxiao0927@gmail.com>
2026-01-25 12:51:30 -0800

    Fix the api proxy for oauth2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,6 +18,10 @@ export default defineConfig({
         target: 'http://localhost:8080',
         changeOrigin: true,
       },
+      '/oauth2': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
     },
   },
   resolve: {
